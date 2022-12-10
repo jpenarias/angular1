86a193eec4a2c879e0838f2c31e6c29b023838d9
jiss <jisethpenarias@gmail.com>
2022-12-09 19:06:58 -0500

    tabala de alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToListComponent } from './to-list/to-list.component';
+import { ChangeNoteColorsPipe } from './change-note-colors.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToListComponent,
+    ChangeNoteColorsPipe
   ],
   imports: [
     BrowserModule
